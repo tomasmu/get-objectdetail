3bcb189b52cadc3939d597fe10e312d4be69580f
tomasmu <tomas.kson@gmail.com>
2019-01-05 01:57:10 +0100

    first commit

---
+++ Get-ObjectDetail.Tests.ps1
@@ -0,0 +1,231 @@
+﻿#by: tomasmu
+#date: 2019-01-05
+
+. ($PSCommandPath -replace '\.Tests\.ps1', '.ps1')
+#Remove-Module GetObjectDetail
+#Import-Module GetObjectDetail
+
+#helper function to make test arrays to compare output with
+function ConvertToCsvRow {
+    param(
+        [parameter(ValueFromPipeline = $true)]$InputObject,
+        [switch]$ArraySyntax
+    )
+
+    process {
+        $InputObject | ForEach-Object {
+                [PSCustomObject]@{
+                    Name  = $_.Name
+                    Value = ($_.Value | ForEach-Object { $_ }) -join ' '
+                    Type  = $_.Type
+                }
+            } |
+            ConvertTo-Csv -NoTypeInformation -Delimiter ',' |
+            ForEach-Object {
+                if ($ArraySyntax) {
+                    "'$($_ -replace "'", "''")'"
+                }
+                else {
+                    $_
+                }
+            } |
+            select -Skip 1
+    }
+}
+
+Describe 'IsEnumerable' {
+    #todo: split into separate tests
+    Context 'Given an IENumerable (array/hashtable/hashset)' {
+        It 'Returns True' {
+            IsEnumerable @() | Should Be $true
+            IsEnumerable @{} | Should Be $true
+            IsEnumerable ([System.Collections.Generic.HashSet[string]]::new()) | Should Be $true
+        }
+    }
+
+    Context 'Given an enumerable type (string) which we want to treat as a value' {
+        It 'Returns False' {
+            IsEnumerable "special case" | Should Be $false
+        }
+    }
+
+    Context 'Given non-enumerable types (null/int/date)' {
+        It 'Returns False' {
+            IsEnumerable $null | Should Be $false
+            IsEnumerable 42 | Should Be $false
+            IsEnumerable (Get-Date) | Should Be $false
+        }
+    }
+}
+
+Describe 'IsDictionary' {
+    Context 'Given a dictionary (hashtable)' {
+        It 'Returns True' {
+            IsDictionary @{ a = 42 } | Should Be $true
+        }
+    }
+
+    Context 'Given a non-dictionary (hashset)' {
+        It 'Returns False' {
+            IsDictionary ([System.Collections.Generic.HashSet[string]]::new()) | Should Be $false
+        }
+    }
+
+    #todo: split into separate tests
+    Context 'Given a non-dictionary (null/array/int/string)' {
+        It 'Returns False' {
+            IsDictionary $null | Should Be $false
+            IsDictionary @('42') | Should Be $false
+            IsDictionary 42 | Should Be $false
+            IsDictionary "str" | Should Be $false
+        }
+    }
+}
+
+Describe 'IsSimple' {
+    #todo: split into separate tests
+    Context "Given a 'simple/value/primitive' type (null/int/string)" {
+        It "Returns True" {
+            IsSimple $null | Should Be $true
+            IsSimple 42 | Should Be $true
+            IsSimple "str" | Should Be $true
+        }
+    }
+
+    Context 'Given complex type (array/hashtable/guid/intarray/timespan)' {
+        It 'Returns False' {
+            IsSimple @('a', 1) | Should Be $false
+            IsSimple @{ 'a' = 1 } | Should Be $false
+            IsSimple (New-Guid) | Should Be $false
+            IsSimple ([int[]](12, 34)) | Should Be $false
+            IsSimple ([timespan]::new(1)) | Should Be $false
+        }
+    }
+}
+
+Describe 'IsIndexable' {
+    #todo: split into separate tests
+    Context 'Given a type which is indexable with integers (string/array/intArray)' {
+        It 'Returns True' {
+            IsIndexable "str" | Should Be $true
+            IsIndexable @('a', 1) | Should Be $true
+            IsIndexable ([int[]](12, 34)) | Should Be $true
+        }
+    }
+
+    Context 'Given a type that cannot be indexable with integers (null/dictionary/hashset/int)' {
+        It 'Returns False' {
+            IsIndexable $null | Should Be $false
+            IsIndexable @{ 'a' = 1 } | Should Be $false
+            IsIndexable ([System.Collections.Generic.HashSet[string]]) | Should Be $false
+            IsIndexable 42 | Should Be $false
+        }
+    }
+}
+
+Describe 'WriteObject' {
+    Context 'Given an object with value' {
+        It 'Outputs the properties we want' {
+            $object = @(12, 34)
+            $expected = [PSCustomObject]@{
+                Name  = "name"
+                Value = "12 34"
+                Type  = "Object[]"
+            } | ConvertToCsvRow
+            
+            WriteObject -Name "name" -InputObject $object | ConvertToCsvRow | Should Be $expected
+        }
+    }
+
+    Context 'Given an object and custom value' {
+        It 'Outputs the properties we want' {
+            $object = @(12, 34)
+            $expected = [PSCustomObject]@{
+                Name = "name"
+                Value = "custom"
+                Type = "Object[]"
+            } | ConvertToCsvRow
+            
+            WriteObject -Name "name" -InputObject $object -CustomValue "custom" | ConvertToCsvRow | Should Be $expected
+        }
+    }
+}
+
+Describe 'Get-ObjectDetail' {
+    Context 'Given int' {
+        It 'Returns expected object' {
+            $object = 42
+            $expected = @(
+                '"$obj","42","Int32"'
+            )
+
+            Get-ObjectDetail -InputObject $object | ConvertToCsvRow | Should Be $expected
+        }
+    }
+
+    Context 'Given string' {
+        It 'Returns expected object and properties' {
+            $object = "this sentence has thirtynine characters"
+            $expected = @(
+                '"$obj","this sentence has thirtynine characters","String"'
+                '"$obj.Length","39","Int32"'
+            )
+
+            Get-ObjectDetail -InputObject $object | ConvertToCsvRow | Should Be $expected
+        }
+    }
+
+    Context 'Given complex object (guid)' {
+        It 'Shows (...) as object value since its content will be expanded later' {
+            $object = Get-Date
+            $expected = @(
+                '"$obj","(...)","Guid"'
+                '"$obj.Guid","0cc4af99-edbc-4813-8106-22971f5f7e75","String"'
+                '"$obj.Guid.Length","36","Int32"'
+            )
+        }
+    }
+
+    Context 'Given object with duplicates' {
+        It 'Marks duplicate complex types as (Duplicate)' {
+            $duplicate = [PSObject]@{ Double = 6.283185 }
+            $notDuplicate = 42
+            $object = [PSCustomObject]@{
+                Duplicate1 = $duplicate
+                Duplicate2 = $duplicate
+                NotDuplicate1 = $notDuplicate
+                NotDuplicate2 = $notDuplicate
+            }
+            $expected = @(
+                '"$obj","(...)","PSCustomObject"'
+                '"$obj.Duplicate1","(...)","Hashtable"'
+                '"$obj.Duplicate2","(Duplicate)","Hashtable"'
+                '"$obj.NotDuplicate1","42","Int32"'
+                '"$obj.NotDuplicate2","42","Int32"'
+            )
+
+            Get-ObjectDetail -InputObject $object -MaxDepth 1 | ConvertToCsvRow | Should Be $expected
+        }
+    }
+
+    Context 'Given int array' {
+        It 'Returns expected object' {
+            $object = [int[]](12, 34)
+            $expected = @(
+                '"$obj","(...)","Int32[]"'
+                '"$obj[0]","12","Int32"'
+                '"$obj[1]","34","Int32"'
+                '"$obj.Count","2","Int32"'
+                '"$obj.Length","2","Int32"'
+                '"$obj.LongLength","2","Int64"'
+                '"$obj.Rank","1","Int32"'
+                '"$obj.SyncRoot","(Duplicate)","Int32[]"'
+                '"$obj.IsReadOnly","False","Boolean"'
+                '"$obj.IsFixedSize","True","Boolean"'
+                '"$obj.IsSynchronized","False","Boolean"'
+            )
+
+            Get-ObjectDetail -InputObject $object | ConvertToCsvRow | Should Be $expected
+        }
+    }
+}
---
+++ Get-ObjectDetail.ps1
@@ -0,0 +1,188 @@
+﻿#by: tomasmu
+#date: 2019-01-05
+
+function IsEnumerable {
+    [CmdletBinding()]
+    param($InputObject)
+
+    $InputObject -is [System.Collections.IEnumerable] -and
+    $InputObject -isnot [string]  #treat [string] as a value
+}
+
+function IsDictionary {
+    [CmdletBinding()]
+    param($InputObject)
+
+    $InputObject -is [System.Collections.IDictionary]
+}
+
+function IsSimple {
+    [CmdletBinding()]
+    param($InputObject)
+
+    $null -eq $InputObject -or
+    $InputObject.GetType().IsPrimitive -or
+    $InputObject -is [string] -or  #treat [string] as a value
+    $InputObject -is [enum]        #treat [enum] as a value, like [int]
+    
+    #$InputObject.GetType().IsValueType #example: [date] is valuetype :(
+}
+
+function IsIndexable {
+    [CmdletBinding()]
+    param($InputObject)
+
+    #$obj and $obj[0] are apparently equal in powershell if $obj is an unindexable datatype
+
+    $null -ne $InputObject -and
+    $null -ne $InputObject[0] -and  #breaks if array has $null element in [0] though :(
+    $InputObject[0].GetHashCode() -ne $InputObject.GetHashCode()
+}
+
+function WriteObject {
+    [CmdletBinding()]
+    param(
+        $InputObject,
+        $Name,
+        $CustomValue
+    )
+
+    $value = if ($null -eq $CustomValue) { $InputObject } else { $CustomValue }
+    $type = if ($null -ne $InputObject) { $InputObject.GetType().Name } else { '$null' }
+
+    $output = [PSCustomObject]@{
+        Name  = $Name
+        Value = $value
+        Type  = $type
+        #Level = $Level
+    }
+
+    $output
+}
+
+function ObjDetail {
+    [CmdletBinding()]
+    param(
+        $InputObject,
+        [string]$Name,
+        [int]$Level,
+        [int]$MaxDepth,
+        [System.Collections.Generic.HashSet[int]]$HashCodes,
+        [string[]]$ExcludeProperty
+    )
+
+    $cmdName = $PSCmdlet.MyInvocation.MyCommand
+    Write-Verbose "$cmdName, $($PSBoundParameters.GetEnumerator() | % { "$($_.Key)='$($_.Value)'" })"
+
+    if ($Level -gt $MaxDepth) {
+        #Write-Error "MaxDepth exceeded: $Level"
+        continue
+    }
+
+    $obj = $InputObject
+    if ($null -eq $obj) {
+        Write-Verbose "$cmdName, $Name is null"
+        WriteObject -Name $Name -InputObject $obj
+    }
+    else {
+        if (-not (IsSimple -InputObject $obj)) {
+            $hashCode = $obj.GetHashCode()
+            $isZeroHash = $hashCode -eq 0 #include anyway, maybe?
+            #$isPSCustomObjectGetHashCodeBug = $obj -is [System.Management.Automation.PSCustomObject] #include anyway, because of duplicate hashcodes?
+            $isUnique = $HashCodes.Add($hashCode) -or $isZeroHash #-or $isPSCustomObjectGetHashCodeBug
+
+            if ($isUnique) {
+                #print complex object without value, properties will be shown later (is this a good idea?)
+                WriteObject -Name $Name -InputObject $obj -CustomValue "(...)"
+            }
+            else {
+                #print object as duplicate:
+                WriteObject -Name $Name -InputObject $obj -CustomValue "(Duplicate)"
+                #Write-Error "Duplicate hashcode detected: $Name = $hashCode"
+                continue
+            }
+        }
+        else {
+            WriteObject -Name $Name -InputObject $obj
+        }
+
+        $ObjDetailParam = @{
+             Level           = $Level + 1
+             MaxDepth        = $MaxDepth
+             HashCodes       = $HashCodes
+             ExcludeProperty = $ExcludeProperty
+        }
+
+        if (IsDictionary -InputObject $obj) {
+            #print with key: $obj['key']
+            Write-Verbose "$cmdName, IsDictionary"
+            foreach ($keyValue in $obj.GetEnumerator()) {
+                $key = $keyValue.Key
+                $value = $keyValue.Value
+                ObjDetail -InputObject $value -Name "$Name['$key']" @ObjDetailParam
+            }
+        }
+        elseif (IsEnumerable -InputObject $obj) {
+            if (IsIndexable -InputObject $obj) {
+                #print with integer index: $obj[index]
+                Write-Verbose "$cmdName, IsEnumerable IsIndexable"
+                for ($index = 0; $index -lt $obj.Count; $index++) {
+                    $value = $obj[$index]
+                    ObjDetail -InputObject $value -Name "$Name[$index]" @ObjDetailParam
+                }
+            }
+            else {
+                #print non-indexable collection (numbered): $obj (#n)
+                #$obj (#n) = ($obj | select -Index n)
+                Write-Verbose "$cmdName, IsEnumerable Not Indexable"
+                $count = -1
+                foreach ($value in $obj) {
+                    $count++
+                    ObjDetail -InputObject $value -Name "$Name ($count)" @ObjDetailParam
+                }
+            }
+        }
+
+        #always print properties: $obj.prop
+        foreach ($prop in $obj.psobject.Properties) {
+            $property = $prop.Name
+            $value = $prop.Value
+            if ($property -notin $ExcludeProperty) {
+                ObjDetail -InputObject $value -Name "$Name.$property" @ObjDetailParam
+            }
+            #else {
+            #    #Write-Verbose "$cmdName, $Name is an ExcludedProperty"
+            #    #WriteObject -Name "$Name.$property" -InputObject $value -CustomValue "(ExcludedProperty)"
+            #}
+        }
+    }
+}
+
+function Get-ObjectDetail {
+    [CmdletBinding()]
+    param(
+        [parameter(ValueFromPipeline = $true)]$InputObject,
+        [string]$Name = '$obj',
+        [int]$MaxDepth = 10,
+        [string[]]$ExcludeProperty
+    )
+
+    begin {
+        $ObjDetailParam = @{
+            Name            = $Name
+            Level           = 0
+            MaxDepth        = $MaxDepth
+            HashCodes       = [System.Collections.Generic.HashSet[int]]::new()
+            ExcludeProperty = $ExcludeProperty
+        }
+    }
+    process {
+        ObjDetail -InputObject $InputObject @ObjDetailParam
+    }
+    end {}
+}
+
+New-Alias -Name god -Value Get-ObjectDetail -ErrorAction SilentlyContinue
+
+#only export functions with dashes
+#Export-ModuleMember '*-*'
